76d308caf14f0681e2a57a9522bf25c2fa9bdf70
FranciscoG <frankjg@gmail.com>
2014-09-17 17:14:49 -0400

    adding fake terminal feature

--- js/site.js
+++ js/site.js
@@ -2,9 +2,17 @@
 var starfield = require('../js/modules/starfield.js');
 var typer = require('../js/modules/text_writer.js');
 
+var terminal = {};
 
+terminal.init = function() {
+  var title = document.getElementById("title");
+  // typer(title, "Welcome to FranciscoG.com");
+};
+
+// http://bililite.com/wvm/cli.html
 window.addEventListener("load", function() {
   starfield.main.init();
+  terminal.init();
 }, false);
 
 window.onresize = function() {
