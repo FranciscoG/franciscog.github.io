a80a9fdd829c8c35ea4489b7f3ca0e9c0187e708
FranciscoG <frankjg@gmail.com>
2014-09-17 16:48:51 -0400

    removed some unused stylus files

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var starfield = require('../js/modules/starfield.js');
 var typer = require('../js/modules/text_writer.js');
 
-
+// http://bililite.com/wvm/cli.html
 window.addEventListener("load", function() {
   starfield.main.init();
 }, false);
