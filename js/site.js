5f2c73cbf1e1fcbba88dd4cbb6b84359a94e9dfa
franciscog <frankjg@gmail.com>
2014-09-19 08:28:25 -0400

    added some comments

--- js/site.js
+++ js/site.js
@@ -10,22 +10,22 @@ function convertCanvasToImage(canvas) {
   return image;
 }
 
-function doit(idata) {
+function imageDataToCanvas(idata) {
   var c = document.createElement('canvas');
   c.width = idata.width;
   c.height = idata.height;
   var ctx = c.getContext('2d');
   ctx.putImageData(idata, 0, 0);
-  ctx.scale(0.4, 0.4);
   return c;
 }
 
 function createNxN(num) {
+  // create an array of NxN
+  // where each item is 1/NxN
   var _tempArr = [];
   for (var i = 0; i < num; i++) {
     _tempArr.push(1 / num);
   }
-  console.log(_tempArr);
   return _tempArr;
 }
 
@@ -36,9 +36,12 @@ terminal.test = function() {
   var options = {
     background: 'undefined',
     onrendered: function(canvas) {
+      // take returned canvas element and convert it into an image
       var newTimg = convertCanvasToImage(canvas);
-      var blurredImg = imgFilters.filterImage(imgFilters.convolute, newTimg, createNxN(9));
-      var newCanvas = doit(blurredImg);
+      // pass this image to image filter library and add blur, returns a canvas image data
+      var blurredImg = imgFilters.filterImage(imgFilters.convolute, newTimg, createNxN(36));
+      // put imageData onto a canvas and append that a div
+      var newCanvas = imageDataToCanvas(blurredImg);
       newCanvas.className = "lastText";
       document.getElementById('terminal').appendChild(newCanvas);
     }
