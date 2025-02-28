9ad91fb4231107668f63487b418caee7660a11ad
franciscog <557684+FranciscoG@users.noreply.github.com>
2025-02-28 00:29:39 -0500

    new build

--- docs/js/theme-switcher.js
+++ docs/js/theme-switcher.js
@@ -1,21 +1,31 @@
-// src: https://web.dev/articles/building/a-theme-switch-component#javascript
+/*
+Color Theme handler
+-------------------
+started from this src:
+https://web.dev/articles/building/a-theme-switch-component#javascript
+*/
 
-class ThemeToggle {
+class ColorTheme {
 	constructor() {
 		this.STORAGE_KEY = "theme-preference";
 		this.LIGHT = "light";
 		this.DARK = "dark";
-		this.selector = "#theme-toggle";
-		this.theme = this.getColorPreference();
+		this.selector = "#color-scheme-select";
+
+		/**
+		 * @type {'auto'|'light'|'dark'}
+		 */
+		this.userSelectedTheme = localStorage.getItem(this.STORAGE_KEY) || "auto";
+		// this.systemTheme = this.getSystemPreference();
 
 		this.reflectPreference = this.reflectPreference.bind(this);
 		this.setPreference = this.setPreference.bind(this);
 
 		window.addEventListener("load", this.onLoad.bind(this));
 
-		window
-			.matchMedia("(prefers-color-scheme: dark)")
-			.addEventListener("change", this.onMatch.bind(this));
+		// window
+		// 	.matchMedia("(prefers-color-scheme: dark)")
+		// 	.addEventListener("change", this.onMatch.bind(this));
 
 		this.reflectPreference();
 	}
@@ -24,40 +34,55 @@ class ThemeToggle {
 		document
 			.querySelector(this.selector)
 			.addEventListener("change", this.themeOnChange.bind(this));
+
+		document.querySelector(this.selector).value = this.userSelectedTheme;
 	}
 
-	onMatch(e) {
-		const isDark = e.matches;
-		this.theme = isDark ? this.DARK : this.LIGHT;
-		this.setPreference();
-	}
+	/**
+	 * System theme changed
+	 * @param {MediaQueryListEvent} e
+	 */
+	// onMatch(e) {
+	// 	this.systemTheme = e.matches ? this.DARK : this.LIGHT;
+	// 	this.reflectPreference();
+	// }
 
-	getColorPreference() {
-		return (
-			localStorage.getItem(this.STORAGE_KEY) ||
-			(window.matchMedia("(prefers-color-scheme: dark)").matches
-				? this.DARK
-				: this.LIGHT)
-		);
-	}
+	// getSystemPreference() {
+	// 	return window.matchMedia("(prefers-color-scheme: dark)").matches
+	// 		? this.DARK
+	// 		: this.LIGHT;
+	// }
 
 	reflectPreference() {
-		document.firstElementChild.setAttribute("data-theme", this.theme);
-		const toggle = document.querySelector(this.selector);
-		if (toggle) {
-			toggle.checked = this.theme === this.DARK;
+		if (this.userSelectedTheme === "auto") {
+			// remove the attribute and let css handle it
+			document.firstElementChild.removeAttribute("data-theme");
+		} else {
+			document.firstElementChild.setAttribute(
+				"data-theme",
+				this.userSelectedTheme
+			);
 		}
 	}
 
 	setPreference() {
-		localStorage.setItem(this.STORAGE_KEY, this.theme);
+		if (this.userSelectedTheme !== "auto") {
+			localStorage.setItem(this.STORAGE_KEY, this.userSelectedTheme);
+		} else {
+			// for auto we remove the key
+			localStorage.removeItem(this.STORAGE_KEY);
+		}
 		this.reflectPreference();
 	}
 
+	/**
+	 * User manually changed the theme
+	 * @param {Event & { target: HTMLSelectElement }} e
+	 */
 	themeOnChange(e) {
-		this.theme = e.target.checked ? this.DARK : this.LIGHT;
+		this.userSelectedTheme = e.target.value;
 		this.setPreference();
 	}
 }
 
-window.themeToggle = new ThemeToggle();
+window.ColorTheme = new ColorTheme();
