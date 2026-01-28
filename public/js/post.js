a08cfe19daf8147f18e3dae1159d9c9bf0781ce4
franciscog <557684+FranciscoG@users.noreply.github.com>
2026-01-28 15:20:57 -0500

    refactor post.js to use event delegation instead of many event listeners for internal link scrolling

--- public/js/post.js
+++ public/js/post.js
@@ -13,10 +13,10 @@
 		}
 
 		if (href && href.startsWith('#')) {
-			event.preventDefault();
 			const targetId = href.substring(1);
 			const targetElement = document.getElementById(targetId);
 			if (targetElement) {
+				event.preventDefault();
 				targetElement.scrollIntoView({ behavior: 'smooth' });
 				history.pushState(null, null, href);
 			}
@@ -24,17 +24,12 @@
 	};
 
 	document.addEventListener('DOMContentLoaded', function () {
-		const links = document.querySelectorAll('.post-content a');
-		for (const link of links) {
-			// if a link href starts with '#', it's an internal link
-			// remove target="_blank" for internal links
-			// Don't really need this anymore because I fixed it in the markdown renderer
-			// but leaving it here just in case
-			const href = link.getAttribute('href');
-			if (href && href.startsWith('#')) {
-				link.removeAttribute('target');
+		const postContent = document.querySelector('.post-content');
+		postContent.addEventListener('click', function (event) {
+			const target = event.target;
+			if (target.tagName === 'A') {
+				scrollToAnchor(event);
 			}
-			link.addEventListener('click', scrollToAnchor);
-		}
+		});
 	});
 })();
